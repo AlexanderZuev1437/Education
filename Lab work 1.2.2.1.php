1405c2a3e02bb0a4b35b87dc1b8d6779da57f65f
Alexander <Zuev>
2017-08-20 12:10:51 +0300

    Novaya Versiya

---
+++ Lab work 1.2.2.1.php
@@ -0,0 +1,24 @@
+<?php
+ /*Lab work 1.2.2.1
+   Create variables with different data types
+ */
+ echo "Lab work 1.2.2.1 ";
+ echo "<br />";
+ echo  'Create variables with different data types';
+ echo "<br />";
+
+ class dog
+ {
+     function golos()
+     {
+         echo 'Gav-Gav';
+     }
+ }
+$d= new dog();
+$M[0] = array(3,null,'apple');
+$M[1]=array ('Apple',$d);
+$M[2]= false;
+var_dump($M)
+
+ ?>
+
