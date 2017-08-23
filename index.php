6bee0176234bf1655e3821e123ee9a737368c70c
Alexander <Zuev>
2017-08-23 03:49:47 +0300

    HomeWork

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
-<meta charset="UTF-8">
-
 <?php
-    echo 'Привет Мир!!!';
 
- dsf?>
+/**
+ * HelloWorld application
+ *
+ * Author: Student Name
+ * Company: BrainAcademy
+ * Date: 24.06.2016
+ */
+
+// Simple string output
+echo 'Hello world';
+
+# New line separator
+print PHP_EOL;
+
+/* Print new string with separator */
+echo 'Hello World again', PHP_EOL;
+
+/*
+ * Kill all next executions and exit the application
+ */
+die('Application end');
